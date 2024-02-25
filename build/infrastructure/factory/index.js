f59a4a787c1e05e4e7824c89d5fad7ac64418ef5
42someone <muhammadjonov.mashkhurbek@gmail.com>
2024-02-25 13:36:52 +0500

    [-] Removed build and added build to gitignore

--- build/infrastructure/factory/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ServerTypes = void 0;
-var ServerTypes;
-(function (ServerTypes) {
-    ServerTypes["Express"] = "EXPRESS";
-})(ServerTypes || (exports.ServerTypes = ServerTypes = {}));
-//# sourceMappingURL=index.js.map
--- build/server/express/middleware/process-file.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProcessFile = void 0;
-function ProcessFile(req, res, next) {
-}
-exports.ProcessFile = ProcessFile;
-//# sourceMappingURL=process-file.js.map
